b2e20549c70e42ac9e9d220ea4451336fbf73df5
AJ Weeks <ajweeks@shaw.ca>
2017-11-24 15:20:25 +0100

    First pass (not working)
    For some reason columns and rows can't be set here,
    so I'm leaving it here for now to come back to later

--- Wipgate/Source/Wipgate/RTS_HUDBase.cpp
+++ Wipgate/Source/Wipgate/RTS_HUDBase.cpp
@@ -1,3 +1,119 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "RTS_HUDBase.h"
+#include "Runtime/Engine/Classes/Engine/Engine.h"
+#include "Runtime/UMG/Public/Components/PanelSlot.h"
+#include "Runtime/UMG/Public/Components/GridSlot.h"
+#include "Runtime/UMG/Public/Components/GridPanel.h"
+#include "Runtime/UMG/Public/Blueprint/WidgetTree.h"
+#include "RTS_UnitCharacter.h"
+
+DEFINE_LOG_CATEGORY(RTS_HUD_BASE_LOG);
+
+void URTS_HUDBase::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::Tick(MyGeometry, InDeltaTime);
+
+	//if (!SelectedUnitsBackgroundImageRef)
+	//{
+	//	UE_LOG(RTS_HUD_BASE_LOG, Error, TEXT("Unit selection background image not set!"));
+	//	return;
+	//}
+
+	if (SelectedUnitIconSize == FVector2D::ZeroVector)
+	{
+		UE_LOG(RTS_HUD_BASE_LOG, Error, TEXT("Selected unit icon size not set!"));
+		return;
+	}
+
+	if (!SelectedUnitIconsHolderRef)
+	{
+		UE_LOG(RTS_HUD_BASE_LOG, Error, TEXT("Selected unit icons holder not set!"));
+		return;
+	}
+
+	// ?
+	SelectedUnitIconsHolderRef->ForceLayoutPrepass();
+
+	FIntPoint viewportSize = GEngine->GameViewport->Viewport->GetSizeXY();
+	FVector2D selectedUnitsBGImageAbsoluteSize = SelectedUnitIconsHolderRef->GetCachedGeometry().GetAbsoluteSize();
+	selectedUnitsBGImageAbsoluteSize.X = FMath::Clamp(selectedUnitsBGImageAbsoluteSize.X, 0.0f, (float)viewportSize.X);
+	selectedUnitsBGImageAbsoluteSize.Y = FMath::Clamp(selectedUnitsBGImageAbsoluteSize.Y, 0.0f, (float)viewportSize.Y);
+
+	FVector2D maxSelectedUnitImageCountF = (selectedUnitsBGImageAbsoluteSize / SelectedUnitIconSize);
+	FIntPoint maxSelectedUnitImageCount = FIntPoint(FMath::TruncToInt(maxSelectedUnitImageCountF.X), FMath::TruncToInt(maxSelectedUnitImageCountF.Y));
+
+	//if (maxSelectedUnitImageCount != m_MaxUnitImageCount)
+	//{
+		m_MaxUnitImageCount = maxSelectedUnitImageCount;
+		UpdateSelectedUnits(SelectedUnitsRef);
+	//}
+}
+
+void URTS_HUDBase::UpdateSelectedUnits(const TArray<ARTS_UnitCharacter*>& SelectedUnits)
+{
+	SelectedUnitsRef = SelectedUnits;
+
+	int32 prevSelectedUnitCount = SelectedUnitIcons.Num();
+	int32 newSelectedUnitCount = SelectedUnitsRef.Num();
+	if (prevSelectedUnitCount > newSelectedUnitCount)
+	{
+		for (int32 i = newSelectedUnitCount; i < prevSelectedUnitCount; ++i)
+		{
+			// Remove now de-selected units' icons from UI
+			SelectedUnitIconsHolderRef->RemoveChildAt(i);
+		}
+	}
+
+	int32 newSelectedUnitIconCount = FMath::Min(newSelectedUnitCount, m_MaxUnitImageCount.X * m_MaxUnitImageCount.Y);
+
+	SelectedUnitIcons.SetNum(newSelectedUnitIconCount);
+
+	for (int32 i = 0; i < SelectedUnitIcons.Num(); ++i)
+	{
+		//if (!IsValid(SelectedUnitIcons[i]))
+		{
+			SelectedUnitIcons[i] = WidgetTree->ConstructWidget<UImage>(UImage::StaticClass());
+			SelectedUnitIconsHolderRef->AddChildToGrid(SelectedUnitIcons[i])->Column = i % 3;
+		}
+	}
+
+	if (newSelectedUnitIconCount > 1)
+	{
+		for (int32 i = 0; i < newSelectedUnitIconCount; ++i)
+		{
+			ARTS_UnitCharacter* selectedUnit = SelectedUnitsRef.GetData()[i];
+
+			UImage* icon = SelectedUnitIcons.GetData()[i];
+			//checkSlow(icon);
+
+			UGridSlot* gridSlot = Cast<UGridSlot>(SelectedUnitIcons.GetData()[i]->Slot);
+			gridSlot->Column = i % m_MaxUnitImageCount.X;
+			gridSlot->Row = i / m_MaxUnitImageCount.X;
+
+			icon->ColorAndOpacity = FLinearColor((float)(gridSlot->Column / m_MaxUnitImageCount.X), 0.0f, 0.0f, 1.0f);
+			
+			/*gridSlot->Padding = FMargin(4.0f);
+			float unitHealthNorm = selectedUnit->m_UnitCoreComponent->CurrentHealth / selectedUnit->m_UnitCoreComponent->BaseHealth;
+			if (unitHealthNorm > 0.0f)
+			{
+				icon->ColorAndOpacity = FMath::Lerp(UnitHealthColor_LowHealth, UnitHealthColor_FullHealth, unitHealthNorm);
+			}
+			else
+			{
+				icon->ColorAndOpacity = UnitHealthColor_Dead;
+			}*/
+
+		}
+	}
+	else if (newSelectedUnitIconCount == 1)
+	{
+
+	}
+	else // No units are selected
+	{
+		// Pass empty array to signal no units are currently selected
+		//TArray<FSelectedUnitIconInfo> selectedUnitInfos = {};
+		//UpdateSelectedUnitIcons(selectedUnitInfos);
+	}
+}
--- Wipgate/Source/Wipgate/RTS_HUDBase.h
+++ Wipgate/Source/Wipgate/RTS_HUDBase.h
@@ -3,11 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/Blueprint/UserWidget.h"
+#include "Runtime/UMG/Public/Components/Image.h"
 #include "RTS_HUDBase.generated.h"
 
 class ARTS_PlayerController;
 class ARTS_UnitCharacter;
+class UGridPanel;
+
+DECLARE_LOG_CATEGORY_EXTERN(RTS_HUD_BASE_LOG, Log, All);
 
 UCLASS(Blueprintable)
 class WIPGATE_API URTS_HUDBase : public UUserWidget
@@ -15,11 +19,18 @@ class WIPGATE_API URTS_HUDBase : public UUserWidget
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintImplementableEvent)
-	void UpdateSelectionBox(FVector2D position, FVector2D size);
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+	void UpdateSelectedUnits(const TArray<ARTS_UnitCharacter*>& SelectedUnits);
+
+	//UFUNCTION(BlueprintImplementableEvent)
+	//void UpdateSelectedUnitIcons(const TArray<FSelectedUnitIconInfo>& SelectedUnitInfos);
+
+	//UFUNCTION(BlueprintImplementableEvent)
+	//void UpdateSelectedUnitIconColor(UImage* Icon, FLinearColor Color);
 
 	UFUNCTION(BlueprintImplementableEvent)
-	void UpdateSelectedUnits(const TArray<ARTS_UnitCharacter*>& selectedUnits);
+	void UpdateSelectionBox(FVector2D position, FVector2D size);
 
 	// TODO: Remove these carefully
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Widgets")
@@ -29,6 +40,36 @@ public:
 	FVector2D SelectionBoxSize;
 
 	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	UGridPanel* SelectedUnitIconsHolderRef = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
 	ARTS_PlayerController* PlayerController = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	TArray<UImage*> SelectedUnitIcons;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	TArray<ARTS_UnitCharacter*> SelectedUnitsRef;
+
+	//UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	//UImage* SelectedUnitsBackgroundImageRef = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	FVector2D SelectedUnitIconSize = FVector2D(32.0f, 32.0f);
+
+	// Unit's icon's color when at full health (blended with Low Health color when health is less than full but not empty)
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	FLinearColor UnitHealthColor_FullHealth;
+
+	// Unit's icon's color when almost dead (blended with Full Helath color when health is less than full but not empty)
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	FLinearColor UnitHealthColor_LowHealth;
+
+	// The color a unit's icon will be when it is dead
+	UPROPERTY(BlueprintReadWrite, Category = "Misc")
+	FLinearColor UnitHealthColor_Dead;
+
+private:
+	FIntPoint m_MaxUnitImageCount;
 
 };
