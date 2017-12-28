3d761ba34198d34734008784945fa30df03bd7f5
WizBoom <brian.van.hyfte@gmail.com>
2017-12-28 16:05:39 +0100

    Add helper classes

---
+++ Wipgate/Source/Wipgate/Helpers/UpgradeHelpers.h
@@ -0,0 +1,49 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EntityHelpers.h"
+#include "UpgradeHelpers.generated.h"
+
+UENUM(BlueprintType)
+enum class EUpgradeType : uint8
+{
+	E_FLAT 				UMETA(DisplayName = "Flat upgrade"),
+	E_PERCENTUAL 		UMETA(DisplayName = "Percentual upgrade")
+};
+
+//MAKE SURE FIRST AND LAST ELEMENT ALWAYS STAY THE SAME
+UENUM(BlueprintType)
+enum class EUpgradeStat : uint8
+{
+	E_SPEED				UMETA(DisplayName = "Speed"),
+	E_DAMAGE			UMETA(DisplayName = "Damage"),
+	E_RATEOFFIRE		UMETA(DisplayName = "Rate of Fire"),
+	E_RANGE				UMETA(DisplayName = "Range"),
+	E_ARMOR				UMETA(DisplayName = "Armor"),
+	E_HEALTH			UMETA(DisplayName = "Health"),
+	E_VISION			UMETA(DisplayName = "Vision")
+};
+
+USTRUCT(BlueprintType)
+struct FUpgrade
+{
+	GENERATED_USTRUCT_BODY()
+
+		//The effect it has on the stat its altering
+		UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		float Effect = 0.f;
+
+	//Type of the upgrade
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EUpgradeType Type = EUpgradeType::E_FLAT;
+
+	//Stat upgrade affects
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EUpgradeStat Stat = EUpgradeStat::E_ARMOR;
+
+	//Unit it affects
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+		EEntityType AffectedType;
+};
