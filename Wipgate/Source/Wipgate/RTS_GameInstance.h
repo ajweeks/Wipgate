70424ca4e7d09ad9a9b6311967335dd59266a2ab
WizBoom <brian.van.hyfte@gmail.com>
2018-01-03 17:32:11 +0100

    Reparent Gameinstance to new GameInstance class

---
+++ Wipgate/Source/Wipgate/RTS_GameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "RTS_GameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIPGATE_API URTS_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
