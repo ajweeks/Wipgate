2f49d531d22a420fe9572cf9358f877482c90950
AJ Weeks <ajweeks@shaw.ca>
2018-01-13 14:13:07 +0100

    Comment out file searching (should prevent error window from appearing)

--- Wipgate/Source/Wipgate/RTS_Cursor.cpp
+++ Wipgate/Source/Wipgate/RTS_Cursor.cpp
@@ -12,13 +12,13 @@ DEFINE_LOG_CATEGORY_STATIC(RTS_Cursor_Log, Log, All);
 URTS_Cursor::URTS_Cursor(const FObjectInitializer& ObjectInitializer) :
 	UUserWidget(ObjectInitializer)
 {
-	AttemptToFindObjectByPath(&DefaultTexture, TEXT("Texture2D'/Game/Art/Cursors/pointyhand.pointyhand'"));
-	AttemptToFindObjectByPath(&GrabbedTexture, TEXT("Texture2D'/Game/Art/Cursors/closedhand.closedhand'"));
-	AttemptToFindObjectByPath(&MoveTexture, TEXT("Texture2D'/Game/Art/Cursors/MoveCursor.MoveCursor'"));
-	AttemptToFindObjectByPath(&AttackMoveTexture, TEXT("Texture2D'/Game/Art/Cursors/AttackCursor2.AttackCursor2'"));
-	AttemptToFindObjectByPath(&DrugTexture, TEXT("Texture2D'/Game/Art/Cursors/DrugCursor.DrugCursor'"));
-	AttemptToFindObjectByPath(&CommanderTexture, TEXT("Texture2D'/Game/Art/Cursors/CommanderCursor.CommanderCursor'"));
-	AttemptToFindObjectByPath(&PotionTexture, TEXT("Texture2D'/Game/Art/Cursors/potioncursor.potioncursor'"));
-	AttemptToFindObjectByPath(&InvalidTexture, TEXT("Texture2D'/Game/Art/Cursors/invalid_cursor.invalid_cursor'"));
-	AttemptToFindObjectByPath(&ShopTexture, TEXT("Texture2D'/Game/Art/Cursors/shop_cursor.shop_cursor'"));
+	//AttemptToFindObjectByPath(&DefaultTexture, TEXT("Texture2D'/Game/Art/Cursors/pointyhand.pointyhand'"));
+	//AttemptToFindObjectByPath(&GrabbedTexture, TEXT("Texture2D'/Game/Art/Cursors/closedhand.closedhand'"));
+	//AttemptToFindObjectByPath(&MoveTexture, TEXT("Texture2D'/Game/Art/Cursors/MoveCursor.MoveCursor'"));
+	//AttemptToFindObjectByPath(&AttackMoveTexture, TEXT("Texture2D'/Game/Art/Cursors/AttackCursor2.AttackCursor2'"));
+	//AttemptToFindObjectByPath(&DrugTexture, TEXT("Texture2D'/Game/Art/Cursors/DrugCursor.DrugCursor'"));
+	//AttemptToFindObjectByPath(&CommanderTexture, TEXT("Texture2D'/Game/Art/Cursors/CommanderCursor.CommanderCursor'"));
+	//AttemptToFindObjectByPath(&PotionTexture, TEXT("Texture2D'/Game/Art/Cursors/potioncursor.potioncursor'"));
+	//AttemptToFindObjectByPath(&InvalidTexture, TEXT("Texture2D'/Game/Art/Cursors/invalid_cursor.invalid_cursor'"));
+	//AttemptToFindObjectByPath(&ShopTexture, TEXT("Texture2D'/Game/Art/Cursors/shop_cursor.shop_cursor'"));
 }
