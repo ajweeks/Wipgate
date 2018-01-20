680a8972c7878f4726dbfa829016dcb3273f705d
Yosha <yosha.vandaele@student.howest.be>
2018-01-20 13:40:55 +0100

    Optimize AI --- Fix health bar overlap with luma bar --- Minimap performance boost --- Make map to store spawners

--- Wipgate/Source/Wipgate/RTS_AIController.cpp
+++ Wipgate/Source/Wipgate/RTS_AIController.cpp
@@ -302,6 +302,10 @@ void ARTS_AIController::RenderFlockingDebug(const FVector separation, const FVec
 
 bool ARTS_AIController::FlockMoveToLocation(const FVector target, const float separationWeight, const float cohesionweight, const float seekWeight, const float avoidanceWeight, const float stepLength, const float acceptanceRadius)
 {
+	m_FlockTick++;
+	if (m_FlockTick % 3 == 0)
+		return false;
+
 	m_FlockCenter = m_Entity->GetActorLocation();
 	StoreNearbyEntities(m_FlockingMoveRadius);
 
@@ -339,6 +343,10 @@ bool ARTS_AIController::FlockMoveToLocation(const FVector target, const float se
 
 bool ARTS_AIController::FlockChaseToLocation(const FVector target, float separationWeight, const float cohesionweight, const float seekWeight, const float avoidanceWeight, const float stepLength, const float acceptanceRadius)
 {
+	m_FlockTick++;
+	if (m_FlockTick % 3 == 0)
+		return false;
+
 	m_FlockCenter = m_Entity->GetActorLocation();
 	StoreNearbyEntities(m_FlockingChaseRadius);
 
